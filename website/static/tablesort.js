1e0061d654bfebc82206693fd2aaef1734706ee5
AndresCavalie <andrescavalie02@gmail.com>
2023-01-12 19:59:36 -0500

    table sorting completing (missing "%")

---
+++ website/static/tablesort.js
@@ -0,0 +1,63 @@
+function containsNumbers(str) {
+    return /[0-9]/.test(str);
+  }
+
+/**
+ * Sorts an HTML table
+ * 
+ * @param {HTMLTableElement} table the table to sort
+ * @param {number} column the index of the column to sort
+ * @param {boolean} asc Determines if the sorting will be in ascending or descending order
+ */
+function sortTableByColumn(table, column, asc = true){
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    //sort each row
+    const sortedRows = rows.sort((a,b) => {
+        const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+
+        if (containsNumbers(aColText) && containsNumbers(bColText)){
+
+            console.log("HAS NUMBERS")
+            
+            return Number(aColText) > Number(bColText) ? (1*dirModifier) : (-1 * dirModifier);
+        }
+        else {
+            console.log("NO NUMBERS")
+            return aColText > bColText ? (1*dirModifier) : (-1 * dirModifier);
+        }
+        
+
+    });
+   
+    while (tBody.firstChild){
+        tBody.removeChild(tBody.firstChild);
+    }
+    
+    tBody.append(...sortedRows);
+
+    //remember how column is currently sorted
+    table.querySelectorAll('th').forEach(th => th.classList.remove('th-sort-asc', 'th-sort-desc'));
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle('th-sort-asc',asc);
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle('th-sort-desc',!asc);
+}
+
+document.querySelectorAll(".table-striped th").forEach(headerCell => {
+
+    headerCell.addEventListener("click", () =>{
+
+        const tableElement = headerCell.parentElement.parentElement.parentElement; 
+        //                                   returns index of this ------------------------------v
+    //                   while looking in here -----------------------------v
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains('th-sort-asc');
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
+
+});
+
+sortTableByColumn(document.querySelector('table'),0)
